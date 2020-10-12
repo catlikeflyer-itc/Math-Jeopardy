6df564f96677bf69df7f0338f7414a382f26d69b
Cabrera <a01025453@itesm.mx>
2020-10-11 20:22:21 -0500

    Added second Frame, now in individual functions
    Two frames now, still need to open them separately, they're in individual functions just to make them easier to tell apart, they don't have to be for the final product.

--- gui.py
+++ gui.py
@@ -1,45 +1,126 @@
 # File that creates a usable GUI for the program
 from tkinter import *
 
-# Initialize the tkinter object along with its size
-root = Tk()
-root.geometry("1000x600")
+def mainFrame():
+    # This is just to separate this frame from the second one,
+    #which has the equation and the answer box
 
-# Frame definition and placements
-mainFrame = Frame(root)
-mainFrame.pack()
+    # Initialize the tkinter object along with its size
+    root = Tk()
+    root.geometry("1000x600")
+    mainFrame = Frame(root)
+    mainFrame.pack()
 
-# Frame label
-nameLabel = Label(mainFrame, text = "Board")
-nameLabel.grid(row=0,columnspan=2)
+    # Frame label
+    nameLabel = Label(mainFrame, text = "Board")
+    nameLabel.grid(row=0,columnspan=10)
 
-# Board buttons for choosing the question
-button1 = Button(mainFrame)
-button2
-button3
-button4
-button5
-button6
-button7
-button8
-button9
-button10
-button11
-button12
-button13
-button14
-button15
-button16
-button17
-button18
-button19
-button20
-button21
-button22
-button23
-button24
-button25
+    # Placeholder text for the buttons, they can change according to the
+    # other code, if we want to make it fancy
 
-# Running the GUI
-root.title("Equation Jeopardy")
-root.mainloop()
+    # Board buttons for choosing the question
+    button1 = Button(mainFrame, text="A1")
+    button1.grid(row=1, column=0)
+
+    button2 = Button(mainFrame, text="A2")
+    button2.grid(row=1, column=1)
+
+    button3 = Button(mainFrame, text="A3")
+    button3.grid(row=1, column=2)
+
+    button4 = Button(mainFrame, text="A4")
+    button4.grid(row=1, column=3)
+
+    button5 = Button(mainFrame, text="A5")
+    button5.grid(row=1, column=4)
+
+    button6 = Button(mainFrame, text="B1")
+    button6.grid(row=2, column=0)
+
+    button7 = Button(mainFrame, text="B2")
+    button7.grid(row=2, column=1)
+
+    button8 = Button(mainFrame, text="B3")
+    button8.grid(row=2, column=2)
+
+    button9 = Button(mainFrame, text="B4")
+    button9.grid(row=2, column=3)
+
+    button10 = Button(mainFrame, text="B5")
+    button10.grid(row=2, column=4)
+
+    button11 = Button(mainFrame, text="C1")
+    button11.grid(row=3, column=0)
+
+    button12 = Button(mainFrame, text="C2")
+    button12.grid(row=3, column=1)
+
+    button13 = Button(mainFrame, text="C3")
+    button13.grid(row=3, column=2)
+
+    button14 = Button(mainFrame, text="C4")
+    button14.grid(row=3, column=3)
+
+    button15 = Button(mainFrame, text="C5")
+    button15.grid(row=3, column=4)
+
+    button16 = Button(mainFrame, text="D1")
+    button16.grid(row=4, column=0)
+
+    button17 = Button(mainFrame, text="D2")
+    button17.grid(row=4, column=1)
+
+    button18 = Button(mainFrame, text="D3")
+    button18.grid(row=4, column=2)
+
+    button19 = Button(mainFrame, text="D4")
+    button19.grid(row=4, column=3)
+
+    button20 = Button(mainFrame, text="D5")
+    button20.grid(row=4, column=4)
+
+    button21 = Button(mainFrame, text="E1")
+    button21.grid(row=5, column=0)
+
+    button22 = Button(mainFrame, text="E2")
+    button22.grid(row=5, column=1)
+
+    button23 = Button(mainFrame, text="E3")
+    button23.grid(row=5, column=2)
+
+    button24 = Button(mainFrame, text="E4")
+    button24.grid(row=5, column=3)
+
+    button25 = Button(mainFrame, text="E5")
+    button25.grid(row=5, column=4)
+
+    # Running the GUI
+    root.title("Equation Jeopardy")
+    root.mainloop()
+
+    return 0
+
+
+def answerFrame():
+    # Second Frame where the answer box is located, does not need to be inside a function
+    #Only to separate it from the first Frame
+    
+    #initialize the frame
+    answerRoot = Tk()
+    answerRoot.geometry("500x300")
+    answerFrame = Frame(answerRoot)
+    answerFrame.pack()
+    
+    #label
+    answerLabel = Label(answerFrame, text="Answer")
+    answerLabel.grid(row=0, columnspan=10)
+    
+    #Answer box and confirm button.
+    #Still need to add the function the button will do once pressed
+    answerButton = Button(answerFrame, text="Submit answer")
+    answerButton.grid(row=3, column=7,columnspan=3)
+    
+    dataEntry = Entry(answerFrame ''', command=function we want it to do''')
+    dataEntry.grid(row=3, column=0, columnspan=4)
+    
+    return 0
