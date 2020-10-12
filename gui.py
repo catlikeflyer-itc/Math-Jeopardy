566b82245734b939d90d431d3690d62be650fd00
Cabrera <a01025453@itesm.mx>
2020-10-11 19:47:19 -0500

    gui.py is the skeleton of the gui so far.
    Only a skeleton, nothing fancy or solid yet. Still needs references to other files as well as many of the necessary objects.

---
+++ gui.py
@@ -0,0 +1,45 @@
+# File that creates a usable GUI for the program
+from tkinter import *
+
+# Initialize the tkinter object along with its size
+root = Tk()
+root.geometry("1000x600")
+
+# Frame definition and placements
+mainFrame = Frame(root)
+mainFrame.pack()
+
+# Frame label
+nameLabel = Label(mainFrame, text = "Board")
+nameLabel.grid(row=0,columnspan=2)
+
+# Board buttons for choosing the question
+button1 = Button(mainFrame)
+button2
+button3
+button4
+button5
+button6
+button7
+button8
+button9
+button10
+button11
+button12
+button13
+button14
+button15
+button16
+button17
+button18
+button19
+button20
+button21
+button22
+button23
+button24
+button25
+
+# Running the GUI
+root.title("Equation Jeopardy")
+root.mainloop()
