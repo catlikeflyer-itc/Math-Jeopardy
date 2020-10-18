407393bace48bab728615de78bac3918902441c1
Cabrera <a01025453@itesm.mx>
2020-10-18 18:12:30 -0500

    revamped gui.py, documented the other two
    gui.py was built again from the ground up, as it was complete and utter shite.
    game.py was documented with comments as to make it easier to navigate and connect to gui.py, although i've not yet done that.
    boards.py was also documented with comments, as to know what does what, and to make the connections to gui.py via game.py easier. Still have to do that.

--- game.py
+++ game.py
@@ -1,39 +1,84 @@
 from boards import GameBoard, Problem
+import time
+import threading
+import sys
+from gui import Board, Answer
 
-# Initialize gameboard
-board = GameBoard()
-show_board = board.generate_display_board()
-secret_board = board.generate_secret_board('equation.csv')
+def game():
+    
+    # initialize the three types of objects there are:
+    # a Board from gui.py, 
+    boardObj = Board()
+    
+    # an Answer from gui.py,
+    answerObj = Answer()
+    
+    # and a GameBoard from boards.py
+    board = GameBoard()
+    
+    # generate the secret_board and display_board atributes 
+    # within the 'board' object instance
+    show_board = board.generate_display_board()
+    secret_board = board.generate_secret_board('equation.csv')
+    
+    # list that holds the solved coordinates
+    solved = []
 
-# Points counter
-points = 0
+    # point counter
+    points = 0
 
-# If max points is reached, stops
-while points < 3:
-    for i in show_board:
-        print(i,'\n')
+    # repeats until points reaches a value of 2
+    while points < 3:
+        
+        # reprints the board each iteration (NEEDS TO CHANGE TO ACCOMODATE THE GUI)
+        for i in show_board:
+            print(i,'\n')
+        
+        # coordinate input, i.e. a1, b5, etc
+        user_choice = input('Enter coordenates: ')
 
-    user_choice = input('Enter coordenates: ')
-    problem = Problem(user_choice.upper(),show_board,secret_board)
+        # repeats if the introduced coordinate is in the solved coordinates list
+        while user_choice in solved:
+            print('Coordenate already solved')
+            
+            # asks the user to input the coordinate again, until it's not one in the list
+            user_choice = input('Enter coordenates: ')     
+        
+        #ASCII check
+        while(len(user_choice) != 2 or ord(user_choice[1]) < 65 or ord(user_choice[1]) > 69 or ord(user_choice[0]) < 49 or ord(user_choice[0]) > 53 ):
+            print(points)
+            user_choice = input('Enter coordenates: ')
+        
+        # adds the inputed coordinate into the solved coordinates list
+        solved.append(user_choice)
 
-    print(problem.problem)
+        # creates a Problem type object from boards.py with the atributes it needs
+        problem = Problem(user_choice.upper(),show_board,secret_board)
 
-    user_answer = int(input('Resultado >>> '))
-    problem.get_answer()
+        # prints the equation to solve (NEEDS TO CHANGE TO ACCOMODATE THE GUI)
+        print(problem.problem)
 
-    if problem.check_answer(user_answer):
-        print(problem.check_answer(user_answer))
-        points += 1
-    else:
-        print(problem.check_answer(user_answer))
+        user_answer = int(input('Resultado >>> '))
+        problem.get_answer()
 
-    board.remove_coord(problem.row, problem.column)
+        # compares the user answer input with the actual answer using 
+        # a method from the class Problem from boards.py
+        if problem.check_answer(user_answer):
+            print(problem.check_answer(user_answer))
+            points += 1
+        else:
+            print(problem.check_answer(user_answer))
 
-print('Ganaste!')
+        # switches the printed coordinate with an x
+        board.remove_coord(problem.row, problem.column)
+    
+    print('Ganaste!')
 
+#threads y timeout
+timeout = 60
+t = threading.Timer(timeout, print, ["bruh, time's up"])
+t.start()
 
+game()
 
-
-
-
-
+#####
